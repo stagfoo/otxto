872c327ce32fbbbda2e22cb58ea94a00efc40b1d
Alex <alexanderddking@gmail.com>
2022-11-05 20:22:58 +0900

    🪴　feature ░ basic wrapper to get files

--- main.py
+++ main.py
@@ -2,54 +2,6 @@ import webview
 
 def load_css(window):
     window.load_css(r"""
-        body,html {
-        margin: 0;
-        padding: 0;
-        background-color: #F4BE9A;
-        color: #d2607e;
-        font-family: silk_flower;
-        font-size: 16vw;
-        position: relative;
-        width:100vw;
-        height:100vh;
-        overflow: hidden;
-    }
-
-    img {
-        width: auto;
-        margin: 0 auto;
-        left: 0;
-        right: 0;
-        top: 0;
-        height: 100%;
-        z-index: 1;
-        position: absolute;
-    }
-
-    button {
-        position: relative;
-        z-index: 2;
-        font-size: 100%;
-        margin: 0vh auto;
-        height: 50vh;
-        width: 100%;
-        text-transform: uppercase;
-        background-color: transparent;
-        border: 0;
-    }
-    button:hover {
-        color: #df7d69;
-        cursor: pointer;
-    }
-    @font-face {
-        font-family: silk_flower;
-        src: url("silk_flower.otf") format("opentype");
-    }
-    * {
-        font-family: silk_flower;
-        color: #d2607e;
-    }
-    
     """)
 
 def evaluate_js(window):
@@ -59,17 +11,15 @@ def evaluate_js(window):
             console.log('@pywebview is ready', pywebview.api)
         })
 
-        function getImage() { pywebview.api.getImage().then((response) => {
-            const fish = document.createElement('img');
-            fish.src = response.result;
-            document.body.appendChild(fish)
-        }) 
+        function getImage() { 
+            pywebview.api.getImage().then((response) => {
+                response.result;
+            }) 
         }
-        function saveFile() { pywebview.api.saveFile("exampleFilename").then((response) => {
-            const fish = document.createElement('div');
-            fish.innerHTML = response.result;
-            document.body.appendChild(fish)
-        }) 
+        function saveFile() { 
+            pywebview.api.saveFile("exampleFilename", "whater").then((response) => {
+                return response.result;
+            }) 
         }
         """
     )
@@ -80,8 +30,12 @@ def load_api(window):
 class Api:
     # function available pywebview.api.X
 
-    def saveFile(self, filename):
-        result = window.create_file_dialog(webview.SAVE_DIALOG, directory='/', save_filename=filename)
+    def saveFile(self, filename, fileContent):
+        result = window.create_file_dialog(webview.SAVE_DIALOG, directory='~/', save_filename=filename)
+        path = "".join(result)
+        file = open(path, 'w')
+        file.write(fileContent)
+        file.close()
         response = {
             'result': result
         }
